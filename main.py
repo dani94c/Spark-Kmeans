d4b30494b67608f6f29ff709c59b64fb9e8c71cc
gerry94 <g.alvaro@studenti.unipi.it>
2020-07-07 15:53:39 +0200

    Added comments

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ def compute_closest(point, cent):
         if temp_dist < min_dist:
             min_dist = temp_dist
             index = j
+	# in this key-value pair index is the key and (1, point) is the value
     return index, (1, point)
 
 
@@ -46,10 +47,12 @@ if __name__ == "__main__":
     sc = pyspark.SparkContext(appName="KMeans", master="yarn")
     sc.setLogLevel("ERROR")
 
+	# deletion of previous output 
     delete_previous_output(output, sc)
 
     # points is an RDD containing the points taken from input_file
     points = sc.textFile(input_file).map(parse_point).persist(pyspark.StorageLevel.MEMORY_AND_DISK)
+	# initial_centroids is an RDD containing the centroids taken from centroid_file
     initial_centroids = sc.textFile(centroid_file).map(parse_point).collect()
     print("Initial centroids:", initial_centroids.collect())
 
@@ -60,13 +63,16 @@ if __name__ == "__main__":
         print("Iteration:", iteration)
         # used a broadcast variable to save centroids values visible on all the worker nodes
         broadcast_centroids = sc.broadcast(initial_centroids)
+		# mapping each point to the closest centroid
         closest_centroid = points.map(lambda x: (compute_closest(x, broadcast_centroids.value)))
+		# sum of the occurrences (0) and of the coordinates (1) of each point reduced by key
         summed_points = closest_centroid.reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))
-        # new_centroids is an ordered array containing the values of the new centroids from the smallest to biggest key
+        # new_centroids is an ordered array (by key) containing the coordinates of new centroids 
         new_centroids = summed_points.map(lambda v: (v[0], v[1][1] / v[1][0])).values().collect()
         print("New centroids:", new_centroids)
 
         shift = 0.0
+		# computation of the euclidian distance between old and new centroids
         for c in range(len(new_centroids)):
             shift += np.sqrt(np.sum((new_centroids[c] - initial_centroids[c]) ** 2))
         print("Shift:", shift)
@@ -76,5 +82,6 @@ if __name__ == "__main__":
         iteration += 1
 
     to_output = sc.parallelize(initial_centroids)
+	# saving the final result in a text file
     to_output.saveAsTextFile(output)
-    
+  
